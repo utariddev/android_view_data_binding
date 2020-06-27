1ebf14cf04643c9c550038e444a57a9e24c3a90d
utarid <utarid@protonmail.com>
2020-06-27 21:20:18 +0300

    view binding yazildi

--- app/src/main/java/com/example/viewbinding/MainActivity.java
+++ app/src/main/java/com/example/viewbinding/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.viewbinding;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.viewbinding.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.buttonTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.textViewTest.setText(binding.editTextTest.getText());
+            }
+        });
     }
 }
